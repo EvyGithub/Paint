49f80550c4fba7e22033bf9779fd65a358aa68a0
Evy <110493699+EvyGithub@users.noreply.github.com>
2024-08-22 12:09:53 -0700

    ok

--- paint.py
+++ paint.py
@@ -1,1 +1,25 @@
+from re import sub
+import sys
 
+if not sys.argv[1]:
+    print("enter something genius")
+    exit()
+
+if sys.argv[1] == "-c":
+    if not sys.argv[2]:
+        print("enter some code genius")
+        exit()
+    code = sys.argv[2]
+else:
+    with open(sys.argv[1], "r") as f:
+        code = sub(r"[^01\+\-><',;.:{}\[\]`~cf]", "", f.read())
+
+def interpret(code):
+    # []
+    loopStack = []
+    for i, item in enumerate(code):
+        if item == "[":
+            loopStack.append(i)
+        elif item == "]":
+            loop = loopStack.pop(-1)
+            # Im lazy pls fix later
